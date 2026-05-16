91c2061637239bb0d8dc87ed9d06f4068f75510b
Alagendran <oxyian@outlook.com>
2026-05-16 20:16:26 +0530

    fix

--- src/utils/proxy.js
+++ src/utils/proxy.js
@@ -2,6 +2,6 @@
 // const PROXY = 'https://victoryworld.in'
 // In development, use empty string to leverage package.json proxy
 // In production, use the full URL
-const PROXY = process.env.NODE_ENV === 'production' ? 'https://victoryworld.in' : ''
+const PROXY = process.env.NODE_ENV === 'production' ? 'https://www.victoryworld.in' : ''
 
 export default PROXY
