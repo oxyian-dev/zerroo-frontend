7c411ffb11efdaaa47b661bef8516d9dbe84a7e7
Alagendran <oxyian@outlook.com>
2026-06-01 05:34:43 +0000

    docs updated

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,14 @@
 const Logo = () => (
   <img
-    src="/brand_logo/web-app-manifest-192x192.png"
-    style={{ width: '100%', height: 'auto', display: 'block' }}
+    src="/brand_logo/web-app-manifest-512x512.png"
+    alt="Victory World"
+    style={{
+      width: '100%',
+      height: 'auto',
+      display: 'block',
+      objectFit: 'contain',
+      imageRendering: 'auto'
+    }}
   />
 )
 
